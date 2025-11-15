e8f107faa56ecc0c98a5e20420a6434f460a3d7a
Arturhk05 <arturhandow@gmail.com>
2025-11-15 19:52:27 -0300

    fix: include HOST in app configuration

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ load_dotenv()
 
 class Config:
     PORT = int(os.getenv('PORT', 5000))
+    HOST = os.getenv('HOST', '127.0.0.1')
     DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
--- run.py
+++ run.py
@@ -5,5 +5,6 @@ app = create_app()
 if __name__ == '__main__':
     port = app.config.get('PORT')
     debug = app.config.get('DEBUG')
+    host = app.config.get('HOST')
 
-    app.run(debug=debug, port=port)
+    app.run(debug=debug, port=port, host=host)
