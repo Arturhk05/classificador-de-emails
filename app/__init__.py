3244573e0cd9b023384c3fa0dc3461997bdf9bfc
Arturhk05 <arturhandow@gmail.com>
2025-11-15 22:38:35 -0300

    feat: add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 
 def create_app(config_class=Config):
     app = Flask(__name__)
 
     app.config.from_object(config_class)
+
+    CORS(app)
 
     from .routes import bp as main_blueprint
     app.register_blueprint(main_blueprint)
